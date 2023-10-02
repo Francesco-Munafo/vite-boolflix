56a6805cd9c51939136571b9b78b82433d25e49b
Francesco-Munafo <acquamarea1@gmail.com>
2023-10-02 12:53:56 +0200

    Fix on axios call error

--- src/store.js
+++ src/store.js
@@ -23,7 +23,7 @@ export const store = reactive({
         axios
             .request({
                 method: 'GET',
-                url: '',
+                url: 'https://api.themoviedb.org/3/search/movie',
                 params: {
                     api_key: '4b353b432307462fb64faa6c35915b3f', //This is a personal API key, if you want to use this code be sure to replace it with your key
                     query: this.query
@@ -39,21 +39,7 @@ export const store = reactive({
                     this.moviesID.push(this.movieResults[i].id);
                 }
                 console.log(this.moviesID);
-                for (let i = 0; i < this.moviesID.length; i++) {
-                    axios
-                    .get(`https://api.themoviedb.org/3/search/movie/${this.moviesID[i]}/credits?api_key=4b353b432307462fb64faa6c35915b3f`)
-                    .then(response =>{
-                        this.movieCast = response.data.cast
-                        console.log(this.movieCast);
-                    })
-
-                    
-                }
-                axios
-                .request({
-                    method: 'GET',
-                    url: `https://api.themoviedb.org/3/movie/45790/credits?api_key=${this.api_key}`
-                })
+                
 
             })
             .catch(error => {
@@ -82,6 +68,11 @@ export const store = reactive({
 
             })
     },
+    /*fetchMovieCast(){
+        for (let i = 0; i < moviesID.length; i++) {
+            .get()
+        }
+    }*/
 
 
 
